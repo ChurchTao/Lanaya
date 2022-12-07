b1855e828851f4e97ca388f6cc176932d2a5337c
churchTao <swkzymlyy@gmail.com>
2022-12-08 00:36:23 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { ref } from "vue";
+import { invoke } from "@tauri-apps/api";
+
+defineProps({
+  msg: String,
+});
+
+const count = ref(0);
+const greet = () => {
+  // 调用命令
+  // 在应用窗口中右键，打开开发者工具
+  // 你会看到控制台上输出了 "Hello, World!"！
+  invoke("greet", { name: "World" })
+    // `invoke` 返回的是一个 Promise
+    .then((response) => console.log(response));
+};
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test HMR
+    </p>
+    <button type="button" @click="greet()">greet</button>
+  </div>
+
+  <p>
+    Check out
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the
+    official Vue + Vite starter
+  </p>
+  <p>
+    Install
+    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+    in your IDE for a better DX
+  </p>
+  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+export default defineConfig({
+  plugins: [vue()],
+  // 防止 vite 输出复杂的 rust 错误
+  clearScreen: false,
+  // Tauri 使用固定端口，若此端口不可用将会导致程序错误
+  server: {
+    strictPort: true,
+  },
+  // 使用 `TAURI_PLATFORM`、`TAURI_ARCH`、`TAURI_FAMILY`,
+  // `TAURI_PLATFORM_VERSION`、`TAURI_PLATFORM_TYPE` 和 `TAURI_DEBUG` 环境变量
+  envPrefix: ["VITE_", "TAURI_"],
+  build: {
+    // Tauri 支持 es2021
+    target: ["es2021", "chrome100", "safari13"],
+    // 不为调试构建压缩构建体积
+    minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
+    // 为调试构建生成源代码映射 (sourcemap)
+    sourcemap: !!process.env.TAURI_DEBUG,
+  },
+});
