9e25e8b791b8a47fd05f742f3b0dfda162f7eb2e
taojiacheng <taojiacheng@dxy.cn>
2022-12-08 14:40:28 +0800

    shadow

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,14 +3,110 @@
     windows_subsystem = "windows"
 )]
 
+use tauri::{Manager, Runtime, SystemTray, SystemTrayEvent, Window};
+use window_shadows::set_shadow;
+mod menu;
+
 #[tauri::command]
-fn greet(name: &str) -> String {
+fn greet(name: String) -> String {
     format!("Hello, {}!", name)
 }
 
 fn main() {
+    let system_tray = SystemTray::new();
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
+        .menu(menu::menu())
+        .setup(|app| {
+            let win = app.get_window("main").unwrap();
+            #[cfg(target_os = "macos")]
+            win.set_transparent_titlebar(true, true);
+
+            set_shadow(&win, true).unwrap();
+            // 监听更新消息
+            win.listen("tauri://update-status".to_string(), move |msg| {
+                println!("New status: {:?}", msg);
+            });
+            Ok(())
+        })
+        .system_tray(system_tray)
+        .on_system_tray_event(|app, event| match event {
+            SystemTrayEvent::LeftClick { .. } => {
+                let win = app.get_window("main").unwrap();
+                if win.is_visible().unwrap() {
+                    win.hide().unwrap();
+                } else {
+                    // win.hide().unwrap();
+                    win.set_focus().unwrap();
+                }
+            }
+            SystemTrayEvent::RightClick {
+                position: _,
+                size: _,
+                ..
+            } => {
+                println!("system tray received a right click");
+            }
+            SystemTrayEvent::DoubleClick {
+                position: _,
+                size: _,
+                ..
+            } => {
+                println!("system tray received a double click");
+            }
+            SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+                "quit" => {
+                    std::process::exit(0);
+                }
+                "hide" => {}
+                _ => {}
+            },
+            _ => {}
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+#[cfg(target_os = "macos")]
+use cocoa::appkit::{NSWindow, NSWindowStyleMask, NSWindowTitleVisibility};
+
+pub trait WindowExt {
+    #[cfg(target_os = "macos")]
+    fn set_transparent_titlebar(&self, title_transparent: bool, remove_toolbar: bool);
+}
+
+impl<R: Runtime> WindowExt for Window<R> {
+    #[cfg(target_os = "macos")]
+    fn set_transparent_titlebar(&self, title_transparent: bool, remove_tool_bar: bool) {
+        unsafe {
+            let id = self.ns_window().unwrap() as cocoa::base::id;
+            NSWindow::setTitlebarAppearsTransparent_(id, cocoa::base::YES);
+            let mut style_mask = id.styleMask();
+            style_mask.set(
+                NSWindowStyleMask::NSFullSizeContentViewWindowMask,
+                title_transparent,
+            );
+
+            if remove_tool_bar {
+                style_mask.remove(
+                    NSWindowStyleMask::NSClosableWindowMask
+                        | NSWindowStyleMask::NSMiniaturizableWindowMask
+                        | NSWindowStyleMask::NSResizableWindowMask,
+                );
+            }
+
+            id.setStyleMask_(style_mask);
+
+            id.setTitleVisibility_(if title_transparent {
+                NSWindowTitleVisibility::NSWindowTitleHidden
+            } else {
+                NSWindowTitleVisibility::NSWindowTitleVisible
+            });
+
+            id.setTitlebarAppearsTransparent_(if title_transparent {
+                cocoa::base::YES
+            } else {
+                cocoa::base::NO
+            });
+        }
+    }
 }
