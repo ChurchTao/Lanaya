abd1a0bee9d2e7ec7bd543cdac8eb73c4313fc77
taojiacheng <taojiacheng@dxy.cn>
2022-12-16 12:42:45 +0800

    config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ["./index.html", "./src/**/*.{vue,js}"],
-  content: [],
+  content: ["./index.html", "./src/**/*.{vue,js}"],
   theme: {
     extend: {},
   },
