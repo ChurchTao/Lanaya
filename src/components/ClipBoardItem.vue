822b9a5dd0d6fca9843f4e605626fa886a2afd8c
taojiacheng <taojiacheng@dxy.cn>
2022-12-16 12:30:55 +0800

    style check

--- src/components/ClipBoardItem.vue
+++ src/components/ClipBoardItem.vue
@@ -1,10 +1,10 @@
 <template>
-  <li class="data-item rounded-sm flex pb-1 relative" role="option" :aria-selected="select">
-    <div class="data-item-outer cursor-pointer rounded-sm shadow-md block pl-4 w-full">
+  <li class="data-item rounded flex pb-1 relative" role="option" :aria-selected="select">
+    <div class="data-item-outer cursor-pointer rounded shadow-md block pl-4 py-4 w-full">
       <div
         class="data-item-container flex flex-row items-center justify-between max-h-28 pr-4 overflow-hidden"
       >
-        <div class="data-item-icon">
+        <div class="data-item-icon w-5 h-5">
           <svg width="20" height="20" viewBox="0 0 20 20">
             <path
               d="M17 6v12c0 .52-.2 1-1 1H4c-.7 0-1-.33-1-1V2c0-.55.42-1 1-1h8l5 5zM14 8h-3.13c-.51 0-.87-.34-.87-.87V4"
@@ -15,8 +15,8 @@
             ></path>
           </svg>
         </div>
-        <div class="data-item-content-wrapper">
-          <span class="data-item-title">
+        <div class="data-item-content-wrapper font-medium relative mx-2">
+          <span class="data-item-title overflow-hidden text-sm">
             <template :key="item.id" v-for="item in data.contentParse">
               <mark v-if="item.match">{{ item.content }}</mark>
               <template v-else>
@@ -25,8 +25,13 @@
             </template>
           </span>
         </div>
-        <div class="data-item-action">
-          <svg class="data-item-select-icon" width="20" height="20" viewBox="0 0 20 20">
+        <div class="data-item-action flex items-center w-5 h-5">
+          <svg
+            class="data-item-select-icon w-4 h-4 hidden"
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+          >
             <g
               stroke="currentColor"
               fill="none"
@@ -58,56 +63,30 @@ defineProps({
 }
 .data-item-container {
   color: var(--docsearch-hit-color);
-  min-height: var(--docsearch-hit-height);
 }
 .data-item-action,
 .data-item-icon {
   color: var(--docsearch-muted-color);
   stroke-width: var(--docsearch-icon-stroke-width);
 }
-
-.data-item-icon {
-  height: 20px;
-  width: 20px;
-}
-
-.data-item-action svg {
-  display: block;
-  height: 18px;
-  width: 18px;
-}
-svg.data-item-select-icon {
-  display: none;
-}
-
 .data-item-content-wrapper {
-  display: flex;
-  flex-direction: column;
-  font-weight: 500;
-  justify-content: center;
-  line-height: 1.4em;
-  margin: 0 8px;
-  overflow-x: hidden;
-  position: relative;
-  text-overflow: ellipsis;
-  /* white-space: nowrap; */
   width: 90%;
-  max-height: var(--docsearch-hit-max-height);
-}
-.data-item-title {
-  font-size: 0.9em;
+  display: -webkit-box;
+  -webkit-box-pack: center;
+  word-break: break-all;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 5;
   overflow: hidden;
+  text-overflow: ellipsis;
 }
-.data-items mark {
+
+.text-sm {
+  line-height: unset;
+}
+
+mark {
   background: none;
   color: var(--docsearch-highlight-color);
-}
-
-.data-item-action {
-  align-items: center;
-  display: flex;
-  height: 22px;
-  width: 22px;
 }
 
 .data-item[aria-selected="true"] .data-item-outer {
